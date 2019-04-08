d585272a39ca493d28dcc4f4437af7c09584da2c
user <you@example.com>
2019-04-08 23:36:54 +0300

    Added task 3 for hw3

---
+++ HW_03/Anton_Asetskiy/anton_asetskiy_hw_03_t_03.rb
@@ -0,0 +1,13 @@
+require 'time'
+
+def task_3(input_string)
+  array_valid_lines = []
+  input_string.each_line do |line|
+    array_valid_lines.push(line) if line.include? 'Calling core with action'
+  end
+  if array_valid_lines.length >= 2
+    (Time.parse(array_valid_lines[-1]) - Time.parse(array_valid_lines[0])).to_s
+  else
+    '0'
+  end
+end
