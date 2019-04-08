a3b57a17627f6304d9f8ac7d9e42abb2a5220cd5
user <you@example.com>
2019-04-08 23:56:14 +0300

    Modified third task to return strings

--- HW_03/Anton_Asetskiy/anton_asetskiy_hw_03_t_03.rb
+++ HW_03/Anton_Asetskiy/anton_asetskiy_hw_03_t_03.rb
@@ -6,8 +6,21 @@ def task_3(input_string)
     array_valid_lines.push(line) if line.include? 'Calling core with action'
   end
   if array_valid_lines.length >= 2
-    (Time.parse(array_valid_lines[-1]) - Time.parse(array_valid_lines[0])).to_s
+    (Time.parse(array_valid_lines[-1]) - Time.parse(array_valid_lines[0])).abs.to_s
   else
     '0'
   end
 end
+
+ts = %q(
+2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+
+  )
+
+puts task_3(ts)
