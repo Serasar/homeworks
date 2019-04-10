a94fa43caaf35b133c96a710c338042e21ec8eb0
user <you@example.com>
2019-04-10 12:19:25 +0300

    Task 1 for hw 4 updated

--- HW_04/Anton_Asetskiy/anton_asetskiy_hw_04_t_01.rb
+++ HW_04/Anton_Asetskiy/anton_asetskiy_hw_04_t_01.rb
@@ -1,13 +1,11 @@
-def task_4_1(number)
-end
+def task_4_1(number, sequence = [1])
+  return [] if number > 1000
+  return [0] if number.zero?
 
-def fib(n, sequence=[1])
-  n = n - 1
-  n.times do
+  number -= 1
+  number.times do
     current_number, last_number = sequence.last(2)
-    sequence << current_number + (last_number or 0)
+    sequence << current_number + (last_number || 0)
   end
   sequence
 end
-
-puts fib(5)
